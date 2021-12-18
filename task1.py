cceb58728c58903136305d1bd22c158acc4d6845
Vlad <vlad.gafarov02@gmail.com>
2021-12-18 09:09:23 +0500

    all tasks

---
+++ task1.py
@@ -0,0 +1,14 @@
+import xml.etree.ElementTree as ET
+import json
+from urllib.request import urlopen
+
+data = urlopen('https://lenta.ru/rss').read().decode('utf8')
+root = ET.fromstring(data)
+news = []
+
+for event in root.findall(r'./channel/item'):
+    date = event.findtext('pubDate')
+    title = event.findtext('title')
+    news.append({'pubDate': date, 'title': title})
+with open('news.json', 'w', encoding='utf-8') as end_file:
+    json.dump(news, fp=end_file, ensure_ascii=False, indent=4)
